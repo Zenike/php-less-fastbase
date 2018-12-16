96a6a87f4d560b702cc84f4b48c559ce3838fa51
Zenike <nicolasmaquet@gmail.com>
2018-12-17 00:36:01 +0100

    replace static assets by node_modules

---
+++ assets/js/monjquery.js
@@ -0,0 +1,1 @@
+console.log("test monjquery php less fastbase");
--- index.php
+++ index.php
@@ -8,23 +8,17 @@
 	<meta http-equiv="Content-Language" content="fr" />
 	<meta http-equiv="Content-Script-Type" content="text/javascript" />
 	<link href="<?php echo $pathLinkFile; ?>css/style.css" rel="stylesheet" type="text/css" />
-	<link href="<?php echo $pathLinkFile; ?>css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 	<link href="<?php echo $pathLinkFile; ?>img/favicon.png" rel="shortcut icon" type="image/png" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 </head>
 
 <body>
 
 <h1>
-	Hellooooo
+	<i class="fas fa-ambulance"></i> Hellooooo
 </h1>
 
-<script type="text/javascript" src="<?php echo $pathLinkFile; ?>js/jquery-3.2.1.min.js"></script>
-<!--[if lt IE 9]>
-    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-<![endif]-->
-<script type="text/javascript" src="<?php echo $pathLinkFile; ?>js/sliker.js"></script>
-<script type="text/javascript" src="<?php echo $pathLinkFile; ?>js/formflex.js"></script>
-<script type="text/javascript" src="<?php echo $pathLinkFile; ?>js/startpack.js"></script>
-<script type="text/javascript" src="<?php echo $pathLinkFile; ?>js/monjquery.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="<?php echo $pathLinkFile; ?>js/js-dist.js"></script>
 </body>
 </html>
